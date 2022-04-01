3aef1f4f020a043db5a9117daad483d7fb805d9f
Ryo Yoshida <Ryo19810902@hotmail.com>
2022-04-01 18:26:42 +0900

    コンフリクトします。

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-alert("here");
+// alert("here");
 
 function clickBtn() {
     let main = document.getElementById("main");
     main.style.color = "red";
+    alert("Red!!");
+    console.log("pushed: red");
 }
 
-console.log("herehere");
+console.log("here");
